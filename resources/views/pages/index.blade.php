1d09030b878bcef6146f37e32aa7552fb81d20a0
Anujcullen <anujbiglow@gmail.com>
2025-12-03 13:01:35 +0530

    test commit

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="jumbotron text-center">
+    <!-- <div class="jumbotron text-center">
     <h1>{{$title}}</h1>
     <p>This is laraval application from the "Laraval series"</p>
-    <p><a class="btn btn-primary btn-lg" href="/login" role="button"
+    <p><a class="btn btn-primary btn-lg" href="/login" role="button" -->
     >login</a> <a class="btn btn-success btn-lg" href="/register" 
     role="button">Register</a></p>
 @endsection 
